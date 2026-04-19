af715b4df8f1719095f6b3fb91844283549ffe8e
cesar arevalo <cesar3.arevalo@gmail.com>
2026-04-19 11:36:21 -0300

    login footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,14 +64,15 @@
         <div id="layoutAuthentication_footer">
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
+                    <div class="text-muted small">
+            Copyright arevalo &copy; Sistema de Ventas 2026 - 2027
+        </div>
+
+        <div class="small">
+            <a href="#">Privacy Policy</a>
+            &middot;
+            <a href="#">Terms &amp; Conditions</a>
+        </div>
                 </div>
             </footer>
         </div>
